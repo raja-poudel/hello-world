9b656963916bf629fbc04625a18e17ba25ab0c75
Raja Poudel <raja.poudel@myanmarwebstore.com>
2022-06-02 14:24:01 +0630

    added Component Library

--- example/src/App.js
+++ example/src/App.js
@@ -1,13 +1,13 @@
 import React from 'react'
 
-import { ExampleComponent } from 'hello-world'
+import { ExampleComponent, Greeting } from 'hello-world'
 import 'hello-world/dist/index.css'
 
 const App = () => {
   return <>
   
   <ExampleComponent text="Create React Library Example 😄" />
-  <h2>Now I am using the created package</h2>
+  <Greeting name="John"/>
   </>
 }
 
--- src/index.js
+++ src/index.js
@@ -1,6 +1,22 @@
 import React from 'react'
 import styles from './styles.module.css'
 
+export const Greeting = ({ name }) => {
+  return (
+    <div style={{
+      height: 100,
+      textAlign: 'center',
+      border: '1px solid grey',
+      margin: 100,
+      lineHeight: 5,
+      backgroundColor: "green",
+      color: 'yellow'
+    }}>
+      Hello {name}
+    </div>
+  )
+}
+
 export const ExampleComponent = ({ text }) => {
   return <div className={styles.test}>Example Component: {text}</div>
 }
